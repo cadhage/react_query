73b69bf3da32826fcb147d28ce8fd0e0790b725e
unknown <cadhage@gmail.com>
2023-05-01 01:40:15 +0530

    adding starter 1

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,8 +1,25 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import SingleItem from "./SingleItem";
+import { useQuery } from "@tanstack/react-query";
+import customFetch from "./utils";
+
+const Items = () => {
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/");
+      return data;
+    },
+  });
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+  if (isError) {
+    return <p style={{ marginTop: "1rem " }}>There was an error...</p>;
+  }
+  // console.log(result.isLoading);
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
